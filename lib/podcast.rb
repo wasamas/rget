19d95429ef33d1c1a8e812dcac6b37e16f590237
TADA Tadashi <t@tdtds.jp>
2014-03-24 18:25:11 +0900

    add support podcast simply, and new sub-command 'rebuild'

---
+++ lib/podcast.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+require 'rss'
+
+class Podcast
+	def initialize(url)
+		@url = url
+		yield self if block_given?
+	end
+
+	def download(name = nil)
+		rss = RSS::Parser.parse(@url)
+		name = rss.channel.title unless name
+		episode = rss.items.first
+		serial = episode.title.scan(/\d+/).flatten.first
+		unless serial
+			puts "fail: recent episode not found."
+			return
+		end
+
+		file = "#{name}##{serial}.mp3"
+		if File.exist? file
+			puts "'#{file}' is existent. skipped."
+			return
+		end
+		print "getting #{serial}..."
+		open(file, 'wb:ASCII-8BIT') do |o|
+			o.write(open(episode.enclosure.url, 'rb:ASCII-8BIT', &:read))
+		end
+		puts "done."
+	end
+end
+
