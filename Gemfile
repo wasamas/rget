5947cfb7412840008e73ee1d66b46efee5b444da
TADA Tadashi <t@tdtds.jp>
2015-02-10 10:39:18 +0900

    use niconico gem to access to nicovideo instead of original code

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in get-webradio2.gemspec
 gemspec
+gem 'niconico', github: 'sorah/niconico'
--- lib/nicovideo.rb
+++ lib/nicovideo.rb
@@ -1,60 +1,9 @@
 require 'webradio'
-require 'mechanize'
+require 'niconico'
 require 'pit'
 require 'pathname'
 require 'open-uri'
 require 'rss'
-
-class NicovideoAPI
-	def initialize(id, pw)
-		@agent = Mechanize::new
-		@id, @pw = id, pw
-	end
-
-	def login(id, pw)
-		@agent.get('https://account.nicovideo.jp/login')
-		@agent.page.forms[0].tap do |form|
-			form.mail_tel = id
-			form.password = pw
-			form.click_button
-		end
-	end
-
-	def metainfo(player_url)
-		uri = "http://ext.nicovideo.jp/api/getthumbinfo/#{video_id(player_url)}"
-		@agent.get(uri)
-		xml = @agent.page.body
-		xml.scan(%r|<thumb>(.*)</thumb>|m).flatten.first.scan(%r|<(.*?)>(.*)</.*?>|).to_h
-	end
-
-	def video_url(player_url)
-		begin
-			@agent.get(player_url)
-			res = @agent.get("http://flapi.nicovideo.jp/api/getflv/#{video_id(player_url)}")
-			raise StandardError.new if res.response['x-niconico-authflag'].to_i == 0
-			CGI.unescape res.body.scan(/(.*?)=(.*?)[&$]/).to_h['url']
-		rescue
-			login(@id, @pw)
-			retry
-		end
-	end
-
-	def download_video(video_url)
-		cookies = @agent.cookie_jar.cookies('http://nicovideo.jp').map{|c|c.to_s}.join(';')
-		uri = URI(video_url)
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.request_get(uri.request_uri, 'Cookie' => cookies) do |res|
-			res.read_body do |body|
-				yield body
-			end
-		end
-	end
-
-private
-	def video_id(player_url)
-		Pathname(URI(player_url).path).basename.to_s
-	end
-end
 
 class Nicovideo < WebRadio
 	def initialize(url)
@@ -62,7 +11,8 @@ class Nicovideo < WebRadio
 			:id => 'your nicovideo id',
 			:pass => 'your nicovideo password'
 		})
-		@nicovideo = NicovideoAPI.new(account[:id], account[:pass])
+		@nico = Niconico.new(account[:id], account[:pass])
+		@nico.login
 		super
 	end
 
@@ -70,9 +20,9 @@ class Nicovideo < WebRadio
 		rss = RSS::Parser.parse(@url)
 		item = rss.items.first
 		player_url = item.link
-		type = @nicovideo.metainfo(player_url)['movie_type']
-		serial = item.title.scan(/#(\d+)/).flatten[0].to_i
-		@file = "#{name}##{'%03d' % serial}.#{type}"
+		video = @nico.video(Pathname(URI(player_url).path).basename.to_s)
+		serial = video.title.scan(/(?:[#第]| EP)(\d+)|/).flatten.compact[0].to_i
+		@file = "#{name}##{'%02d' % serial}.#{video.type}"
 		if File.exist? @file
 			puts "'#{@file}' is existent. skipped."
 			return
@@ -80,8 +30,7 @@ class Nicovideo < WebRadio
 
 		print "getting #{serial}..."
 		open(@file, 'wb:ASCII-8BIT') do |o|
-			video_url = @nicovideo.video_url(player_url)
-			@nicovideo.download_video(video_url) do |body|
+			video.get_video do |body|
 				print '.'
 				o.write(body)
 			end
